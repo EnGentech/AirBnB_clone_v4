d32784d9137a6a955f45665dd709ceacdb955d69
EnGentech <engen.inyang@gmail.com>
2023-08-06 15:39:09 +0100

    created 1-hbnb route

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    let my_list = []
+    
+  $('li input:checkbox').on('change', function(){
+    if ($(this).is(':checked')){
+
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+        
+        my_list.push(amenityName)
+        
+        $('.amenities h4').text(my_list.join(', '))
+        
+    }
+    else if (!$(this).is(':checked')){
+
+        const amenityName = $(this).data('name');
+        let index = my_list.indexOf(amenityName)
+        if (index !== -1){
+            my_list.splice(index, 1)
+        }
+        
+        $('.amenities h4').text(my_list)
+    }
+
+    })
+})
